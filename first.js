3dc41327cb48f8633ddcaa4898fcdc47b44e8718
amitkumar.cs19@bmsce.ac.in <amitkumar.cs19@bmsce.ac.in>
2022-03-05 14:35:27 +0530

    commit all changes

--- first.js
+++ first.js
@@ -3,12 +3,32 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("drop").style.top = "0";
-    document.getElementById("dropbuttons").style.top="82px";
+    document.getElementById("dropbtn").style.top="60px";
     
   } else {
-    document.getElementById("drop").style.top = "-82px";
-    document.getElementById("dropbuttons").style.top="0";
+    
+   document.getElementById("dropbtn").style.top="-60px";
   }
   prevScrollpos = currentScrollPos;
 } 
+
+var btn1=document.getElementById("getBtn");
+btn1.addEventListener("click",()=>{
+  console.log("mouse over btn 1");
+})
+
+function loadDoc()
+{
+  var xhttp=new XMLHttpRequest();
+  xhttp.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200)
+    {
+      document.getElementById("content").innerHTML=this.responseText;
+
+    }
+  };
+  xhttp.open("GET","amit.txt",true);
+  xhttp.send();
+
+
+}
